6111afface15455d18856dc3dbe5fbc11e612f8f
damithchandrasekara <damithkum565@gmail.com>
2018-05-05 17:15:04 +0530

    Project compiled successfully

---
+++ onlineclinicbackend/src/main/java/damith/chandrasekara/onlineclinicbackend/App.java
@@ -0,0 +1,13 @@
+package damith.chandrasekara.onlineclinicbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
